491c20720c83d48a37546606bb6bd035a380e0c7
Vincent Kranendonk <kranendonk.vincent@gmail.com>
2015-11-22 10:44:29 +0100

    Added sidebar functionality - Sage setting is now in lib/setup

--- lib/timber.php
+++ lib/timber.php
@@ -1,24 +1,51 @@
 <?php
+use Roots\Sage\Setup;
 
 /**
  * Timber
- *
- * Data
  */
 
-add_filter('timber_context', 'add_to_context');
+//add_filter('timber_context', 'add_to_context');
+//
+//function add_to_context($data){
+//
+//    /* Add extra data */
+//    $data['foo'] = 'I am some other typical value set in your functions.php file, unrelated to the menu';
+//
+//    /* Menu */
+//    $data['menu'] = new TimberMenu('primary_navigation');
+//
+//    return $data;
+//
+//}
 
-function add_to_context($data){
 
-    /* Add extra data */
-    $data['foo'] = 'I am some other typical value set in your functions.php file, unrelated to the menu';
 
-    /* Menu */
-    $data['menu'] = new TimberMenu('primary_navigation');
+class TwigSageTheme extends TimberSite {
+    function __construct() {
+        add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+        parent::__construct();
+    }
+    function add_to_context( $context ) {
 
-    return $data;
+        /* Add extra data */
+        $data['foo'] = 'I am some other typical value set in your functions.php file, unrelated to the menu';
 
+        /* Menu */
+        $data['menu'] = new TimberMenu('primary_navigation');
+
+        /* Site info */
+        $context['site'] = $this;
+
+        /* Site info */
+        $context['display_sidebar'] = Setup\display_sidebar();
+
+        $context['sidebar_primary'] = Timber::get_widgets('sidebar-primary');
+
+        return $context;
+    }
 }
+new TwigSageTheme();
 
 /**
  * Timber
